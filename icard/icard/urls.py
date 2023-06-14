cb1f7162d4dd4a3173a3bb8317cc31d5d282f956
Alander Gimenez <82914806+alandergimenez@users.noreply.github.com>
2023-06-14 13:05:33 +0200

    Swagger instalado

--- icard/icard/urls.py
+++ icard/icard/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Restaurant",
+      default_version='v1',
+      description="Documentación de mi APP",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="alander.uk@shotmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
