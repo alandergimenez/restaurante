24f57afe7862523e112721b2ca9ef33ef500ef94
Alander Gimenez <82914806+alandergimenez@users.noreply.github.com>
2023-06-22 18:59:16 +0200

    Formulario

--- icard/icard_react/src/components/Admin/LoginForm/LoginForm.js
+++ icard/icard_react/src/components/Admin/LoginForm/LoginForm.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Button, Form } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import './LoginForm.scss';
 
 export function LoginForm() {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+    });
+
     return (
         <div>
             <h1>Login Form</h1>
             <form className="login-form-admin">
-                <Form.Input name = 'email' placeholder='Correo electrónico' />
-                <Form.Input name = 'password' type='password' placeholder='Contraseña' />
+                <Form.Input name = 'email' placeholder='Correo electrónico' value={formik.values.email} onChange={formik.handleChange} />
+                <Form.Input name = 'password' type='password' placeholder='Contraseña' value={formik.values.password} onChange={formik.handleChange} />
                 <Button type="submit" content='Iniciar sesión' primary fluid />
             </form>
         </div>
     )
+}
+
+function initialValues(){
+    return {
+        name: '',
+        password: '',
+    };
 }
